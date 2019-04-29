1052eb19e590425b1757769d8eb9f0a278491f87
Franco Melandri <franco.melandri@gmail.com>
2019-04-29 14:24:15 +0200

    adding Option testing

---
+++ src/main/java/it/fmelandri/vavr/App.java
@@ -0,0 +1,12 @@
+package it.fmelandri.vavr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    }
+}
---
+++ src/test/java/it/fmelandri/vavr/OptionTest.java
@@ -0,0 +1,40 @@
+package it.fmelandri.vavr;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import io.vavr.control.Option;
+
+public class OptionTest 
+{
+    @Test
+    public void ShouldNotmapItemIfNone()
+    {
+        Option<String> optional = Option.of(null);
+        optional.map(value -> {assertTrue(false); return value; });
+        assertTrue( true );
+    }
+
+    @Test
+    public void ShouldmapItemIfSome()
+    {
+        Option<String> optional = Option.of("some");
+        optional.map(value -> {assertEquals("some", value); return value; });
+        assertTrue( true );
+    }
+
+    @Test
+    public void ShouldGetElseIfNone()
+    {
+        Option<String> optional = Option.of(null);        
+        assertEquals("else", optional.getOrElse("else"));
+    }
+
+    @Test
+    public void ShouldNotGetElseIfSome()
+    {
+        Option<String> optional = Option.of("some");        
+        assertEquals("some", optional.getOrElse("else"));
+    }
+}
